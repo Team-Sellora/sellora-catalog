90181d6812ff0edc0f2ab1775650bccce119bb7e
NaveenTharuka <jnaveen829@gmail.com>
2026-09-06 17:44:50 +0530

    CSP-146 feat: add product price history schema

--- src/Sellora.CatalogService.Infrastructure/Persistence/CatalogDbContext.cs
+++ src/Sellora.CatalogService.Infrastructure/Persistence/CatalogDbContext.cs
@@ -18,6 +18,8 @@ public class CatalogDbContext : DbContext
 
     public DbSet<Product> Products => Set<Product>();
     public DbSet<ProductBatch> ProductBatches => Set<ProductBatch>();
+    public DbSet<ProductPriceHistory> ProductPriceHistory =>
+        Set<ProductPriceHistory>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -35,6 +37,11 @@ public class CatalogDbContext : DbContext
             .HasQueryFilter(batch =>
                 _tenantContext.CompanyId != null &&
                 batch.CompanyId == _tenantContext.CompanyId);
+
+        modelBuilder.Entity<ProductPriceHistory>()
+            .HasQueryFilter(history =>
+                _tenantContext.CompanyId != null &&
+                history.CompanyId == _tenantContext.CompanyId);
     }
 
 
